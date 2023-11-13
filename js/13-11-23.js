cd5104d792d93c41971ecf471b7200acaf403b9c
nephriteagain <oneabove232@gmail.com>
2023-11-13 14:28:38 +0800

    13-11-23 && 12-11-23

---
+++ js/13-11-23.js
@@ -0,0 +1,27 @@
+"use strict";
+/**
+ * This problem was asked by Stripe.
+
+Given an array of integers,
+find the first missing positive integer in
+linear time and constant space.
+In other words, find the lowest positive integer
+that does not exist in the array.
+The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
+ */
+function solution(numbers) {
+    var minpos = 1;
+    for (var i = 0; i < numbers.length; i++) {
+        if (numbers[i] === minpos) {
+            minpos++;
+        }
+    }
+    return minpos;
+}
+var s1 = solution([3, 4, -1, 1]);
+var s2 = solution([1, 2, 0]);
+console.log(s1, s2);
