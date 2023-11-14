4d6262471a4d3d92c6d26791ab6daeafd22b9b52
nephriteagain <oneabove232@gmail.com>
2023-11-14 10:47:21 +0800

    14-11-23

---
+++ 14-11-23.ts
@@ -0,0 +1,32 @@
+/**
+ * This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) 
+returns the first and last element of that pair. 
+For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+ */
+
+type pair = [ a: any, b: any ]
+
+function cons(a:any,b:any) : pair {
+    return [a, b, ] 
+}
+
+function car(cons: pair) {
+    return cons[0]
+}
+
+function cdr(cons:pair) {
+    return cons[1]
+}
+
+console.log(car(cons(3,4)))
+console.log(cdr(cons(3,4)))
