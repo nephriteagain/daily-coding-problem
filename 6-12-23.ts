b8d0b50251d4689a1e67cd6f04b54b50b706807c
nephriteagain <oneabove232@gmail.com>
2023-12-06 11:30:23 +0800

    6-12-23

---
+++ 6-12-23.ts
@@ -0,0 +1,50 @@
+/**
+ * Given a string of round, curly, 
+ * and square open and closing brackets, 
+ * return whether the brackets are balanced (well-formed).
+
+For example, given the string "([])[]({})", you should return true.
+
+Given the string "([)]" or "((()", you should return false.
+ */
+
+(function(){
+    
+
+
+    function validBrackets(brackets:string) : boolean {
+        const stack : string[] = [];        
+        
+
+        for (let i = 0; i < brackets.length; i++) {
+            if (brackets[i] === '(' || brackets[i] === '[' || brackets[i] === '{') {
+                stack.push(brackets[i])
+            }
+            else if (stack.length === 0) {
+                return false
+            }
+            else if (brackets[i] === ')' && stack.pop() !== '(') {
+                return false
+            }
+            else if (brackets[i] === '}' && stack.pop() !== '{') {
+                return false
+            }
+            else if (brackets[i] === ']' && stack.pop() !== '[') {
+                return false
+            }
+
+
+        }
+
+        if (stack.length !== 0) {
+            return false
+        }
+
+        return true
+    }
+
+    console.log(validBrackets("([])[]({})"))
+    console.log(validBrackets("([)]"))
+    console.log(validBrackets("((()"))
+
+})()
